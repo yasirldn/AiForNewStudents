61dd7b4f9befe42535aac4cb49534162167879b1
yasirldn <119271926+yasirldn@users.noreply.github.com>
2025-02-20 09:13:06 +0000

    This is the prototype

--- UoWAssistant.py
+++ UoWAssistant.py
@@ -1,29 +1,56 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 # Streamlit Page Config
 st.set_page_config(page_title="Watson Assistant", page_icon="🤖", layout="wide")
 
-# Streamlit UI
-st.title("Uow Assistant Chatbot")
-st.write("The Chatbot is in Bottom Right Corner")
 
-# Watson Assistant Chatbot Script
-watson_script = """
-<script>
-    window.watsonAssistantChatOptions = {
-        integrationID: "b7b7c8ae-f11e-4bdd-a1b3-6bee7697de11",
-        region: "eu-gb",
-        serviceInstanceID: "1be8a1d9-b9c1-4017-ad84-d1858b6d1605",
-        onLoad: async (instance) => { await instance.render(); }
-    };
-    setTimeout(function(){
-        const t=document.createElement('script');
-        t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-        document.head.appendChild(t);
-    });
-</script>
-"""
 
-# Embed the chatbot script using Streamlit components
-components.html(watson_script, height=600)
+
+# --- setting up new pages links ---
+
+Chatbot = st.Page (
+    "pages/UoWAssistantChatbot.py",
+    title="Welcome to UoW Assistant Chatbot",
+    icon=":material/account_circle:",
+
+
+
+)
+Welcome = st.Page(
+    "pages/Welcome.py",
+    title="Welcome to UoW Assistant Web App",
+    icon=":material/account_circle:",
+)
+
+about = st.Page(
+    "pages/about.py",
+    title="About the ChatBot",
+    icon=":material/account_circle:",
+    default=True,
+)
+exams = st.Page(
+    "pages/exams.py",
+    title="Exam FAQs",
+    icon=":material/bar_chart:",
+)
+attendance = st.Page(
+    "pages/attendance.py",
+    title="Attendance FAQs",
+    icon=":material/smart_toy:",
+)
+
+
+# ---navbar-------
+
+nav = st.navigation(
+    {
+        "Chatbot": [Chatbot],
+        "Info": [about],
+        "Projects": [exams, attendance],
+    }
+)
+nav.run()
+#-----
+
+st.sidebar.markdown("Made by Muhammad Yasir")
+
